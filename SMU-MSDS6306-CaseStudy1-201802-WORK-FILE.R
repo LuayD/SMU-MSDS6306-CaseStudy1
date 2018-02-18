b1d719bd6fe256099bac2f5c12e8173b5d9c28ab
Luay Dajani <luayd@hotmail.com>
2018-02-17 18:52:33 -0600

    Luay - First Commit for seed files with basic information

---
+++ SMU-MSDS6306-CaseStudy1-201802-WORK-FILE.R
@@ -0,0 +1,19 @@
+##
+# SMU MSDS 6306 Case Study 1: Beers, Breweries, and Bitterness
+# Base R Workfile for Case Study
+# Work by: Tiger Team - Adil Siraj, Garrett Mozey, Dana Geislinger, Luay Dajani
+##
+
+## Analysis 1: Number of Breweries per US State
+
+## Analysis 2: Merge of Beer & Breweries Data
+
+## Analysis 3: Missing Data (NA's in each column)
+
+## Analysis 4: Compute median alcohol content (ABV) and international bitterness unit (IBU) per each state.
+
+## Analysis 5: Show which state has the maximum alcoholic (ABV) beer and well as the most bitter.
+
+## Analysis 6: Summary of statistics for the alcohol content (ABV).
+
+## Analysis 7: Prove relationship between the bitterness of the beer nad its alcoholic content/
